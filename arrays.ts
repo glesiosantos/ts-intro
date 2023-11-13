8ef5648b474b1bc71433d5901b3eb9a37888a4bd
glesiosantos <glesioss@gmail.com>
2023-11-13 14:10:30 -0300

    treinamento

---
+++ arrays.ts
@@ -0,0 +1,14 @@
+let numeros: number[] = [2.5, 2, 0.1]
+let nomes: Array<string> = ['José', '2', 'Maria', 'Francis']
+
+let nome: string
+nome = 'Jean'
+
+
+nomes.push(nome) // add elemento na lista
+
+nomes.splice(1, 1)
+numeros.pop()
+console.log(nomes[0])
+console.log(numeros)
+console.log(nomes)
---
+++ index.ts
@@ -0,0 +1,5 @@
+let a: number = 10
+let b: number = 5
+let c: number = a + b
+
+console.log(`A soma de a + b é ${c}`)
